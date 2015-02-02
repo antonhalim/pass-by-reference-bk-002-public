0ad65bcb6a4cbfc87ca2698f68248a3750383f3a
axton21 <axton_n@yahoo.com>
2015-02-02 11:10:26 -0500

    labs

--- lib/pass_by_reference.rb
+++ lib/pass_by_reference.rb
@@ -8,9 +8,15 @@ def add_instructor(instructors, name)
 	instructors << name
 end
 
-instructors
-be_friend = []
-def be_friend_with(play_pals, name)
-	be_friend = play_pals << name
-	be friend
+# instructors
+def be_friends_with(play_pals, name)
+	be_friend = []
+	be_friend << play_pals
+	be_friend << name
+	be_friend.flatten!
+
+	# # print be_friend
 end
+# be_friend_with(play_pals, "Avi")
+
+# puts play_pals
