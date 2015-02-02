4b2aded83ee54c71f5aac80990f0b7f11ce95cd4
axton21 <axton_n@yahoo.com>
2015-02-02 10:37:11 -0500

    finishing the labs [the best feeling in the world]

---
+++ lib/pass_by_reference.rb
@@ -0,0 +1,16 @@
+
+ instructors = ["Steven", "Avi"]
+play_pals = ["Steven"]
+
+
+
+def add_instructor(instructors, name)
+	instructors << name
+end
+
+instructors
+be_friend = []
+def be_friend_with(play_pals, name)
+	be_friend = play_pals << name
+	be friend
+end
